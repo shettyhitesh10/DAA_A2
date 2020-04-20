f228431d18c12874a3f9c476431b68989c51b7ef
Hitesh <shettyhitesh10@gmail.com>
2020-04-20 16:39:20 +0530

    task3 max node limit is increased to 10^4 to 10^5

--- task3bipartite.cpp
+++ task3bipartite.cpp
@@ -1,9 +1,15 @@
+// #include<iostream>
+// #include<fstream>
+// #include<limits.h>
+// #include<string.h>
+// #include<queue>
 #include<bits/stdc++.h>
-#define MAX_NODES 500
+#define MAX_NODES 1000000000
+
 
 using namespace std;
 
-int augment(int Gf[][MAX_NODES], int V, int f, int P[], int s, int t) {
+int augment(vector<vector<int> > &Gf, int V, int f, vector<int> &P, int s, int t) {
     ///let b = bottleneck(P,f)
     int b = MAX_NODES;
     ///finding b
@@ -23,7 +29,7 @@ int augment(int Gf[][MAX_NODES], int V, int f, int P[], int s, int t) {
     return f;
 }
 
-bool isThereAPath(int s, int t, int P[], int Gf[][MAX_NODES], int V) {
+bool isThereAPath(int s, int t, vector<int> &P, vector<vector<int> > &Gf, int V) {
     bool visited[V];
     for(int i=0; i<V; i++) visited[i]=0;
     
@@ -47,18 +53,18 @@ bool isThereAPath(int s, int t, int P[], int Gf[][MAX_NODES], int V) {
     return visited[t];
 }
 
-int fordFulkerson(int G[][MAX_NODES], int V, int s, int t) {
-    
-    ///initially f(e) = 0 for all e in G
+int fordFulkerson(vector<vector<int> > &G, int V, int s, int t) {
+	
+	///initially f(e) = 0 for all e in G
     int f=0;
     ///making the residual graph Gf
-    int Gf[MAX_NODES][MAX_NODES];
-    for(int i=0; i<V+1; i++)
-        for(int j=0; j<V+1; j++)
+    vector<vector<int> > Gf(V, vector<int>(V));
+    for(int i=0; i<V; i++)
+        for(int j=0; j<V; j++)
             Gf[i][j]=G[i][j];
             
     /// Let P be a simple s-t path in Gf
-    int P[V];
+    vector<int> P(V);
             
     ///while there is an s-t path in the residual graph Gf
     while(isThereAPath(s, t, P, Gf, V)) {
@@ -73,37 +79,33 @@ int fordFulkerson(int G[][MAX_NODES], int V, int s, int t) {
 int main() {
     
     int V,n;
-    
+	int s,t;
+	
     ///cin input part. To enter input manually. comment the file stream part and uncomment the cin input part
-    cout<<"Please enter the number of vertices and edges \nthen the paths(first vertex is 1)\n";
-    cin >> V >> n;
-
-    int G[MAX_NODES][MAX_NODES];
-    for(int i = 0; i < V+2; i++)
-        for(int j = 0; j < V+2; j++)//V+1 should be enough V+2 is just for testing
-            G[i][j]=0;
-    int s,t;
-    
-    
-    
+	cin >> V >> n;
+	//cin>>s>>t;
     s=0;
     t=V+1;
 
-
+    vector<vector<int> > G(V+2,vector<int> (V+2));
+	
     //connecting s and t to the bipartite graph
+
     for(int i=1; i<=V/2; i++) {
         G[0][i] = 1;
-        G[V/2+i][V+1] = 1;
+        G[ceil(V/2.0)+i][V+1] = 1;
+
     }
 
+    G[(V/2)+1][V+1] = 1;
+    //NOTE : Odd input nodes results in uneven distribution (k,k+1 type)
+
     for(int i=0; i<n; i++) {
-        int a,b;
+        int a,b,c;
         cin>>a>>b;
         G[a][b] = 1;
     }
-
     
-
     ///file stream part. To enter input through file. uncomment the file stream part and comment the cin input part
     
     // fstream myfile;
@@ -111,6 +113,7 @@ int main() {
     // string word;
     // int wNo = 1;
     // int a,b,c;
+    // vector<vector<int>> G(MAX_NODES,vector<int> (MAX_NODES));
     // while(myfile >> word) {
     //     if(wNo==1) {
     //         V=stoi(word);
@@ -139,18 +142,16 @@ int main() {
     //     }
     // }
     // myfile.close();
-    
-
-    int ans = fordFulkerson(G, V+2, s, t);//The Total nodes is increased by 2 because of sink and source.
+	
+	int ans = fordFulkerson(G, V+2, s, t);//The Total nodes is increased by 2 because of sink and source.
     cout <<"answer : " << ans;
-    return 0;
+	
+	return 0;
 }
-
 
 /*
 TEST CASES for task 3:(number : anwer)
 //1:5
-
 12 8 
 1 7 
 1 8 
@@ -162,7 +163,6 @@ TEST CASES for task 3:(number : anwer)
 6 12 
 
 //2 : 5
-
 10 9
 1 6 
 1 7 
@@ -174,7 +174,7 @@ TEST CASES for task 3:(number : anwer)
 4 10 
 5 10 
 
-//3
+//3 : 9
 20 20
 1 11
 2 12
@@ -191,58 +191,4 @@ TEST CASES for task 3:(number : anwer)
 1 14
 1 15
 1 16
-1 17
-1 18
-1 19
-2 11
-1 20
-
-TEST CASES for task 1:(number : anwer)
-//1: 5
-14 20
-0 13
-0 1 1
-0 2 1
-0 3 1
-0 4 1
-0 5 1
-0 6 1
-1 7 1
-1 8 1
-3 7 1
-3 10 1
-4 9 1
-5 9 1
-5 10 1
-6 12 1
-7 13 1
-8 13 1
-9 13 1
-10 13 1 
-11 13 1
-12 13 1
-
-//2: 5
-12 19
-0 11
-0 1 1
-0 2 1
-0 3 1
-0 4 1
-0 5 1
-1 6 1
-1 7 1
-2 6 1
-3 7 1
-3 8 1
-4 8 1
-4 9 1
-4 10 1
-5 10 1
-6 11 1
-7 11 1
-8 11 1
-9 11 1
-10 11 1
-
 */
